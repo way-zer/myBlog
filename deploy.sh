fe8fae4b2c703713b382f1f6214e5579205ecd19
way-zer <himc.wicp@gmail.com>
2020-02-29 00:15:29 +0800

    Add Support for Iterable Type Add TypeVariableProvider to give variables to vars of the type

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 ng build --prod
 cd dist/MyBlog
+echo "www.wayzer.cf" > CNAME
 git init
 git add .
+git commit -m "deploy"
 git remote add origin git@github.com:way-zer/myBlog.git
 git push -u origin master:gh-pages --force
 
